abb519338f057057e5c28515ca0651e81f2ec946
mogghwhy <112501265+mogghwhy@users.noreply.github.com>
2026-03-25 00:16:36 +0200

    parametrize config file name

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ from module.config_utils import find_region_cfg
 
 def main():
 
-    if len(sys.argv) != 2:
-        print("Usage: python main.py <path/to/snapshots>")
+    if len(sys.argv) != 3:
+        print("Usage: python main.py <path/to/snapshots> <config_file_name>")
         sys.exit(1)
 
-    snapshots_root = sys.argv[1]  # first argument after script name
-    all_cfgs = find_region_cfg(snapshots_root, "region.cfg")
+    snapshots_root = sys.argv[1]    # first argument after script name
+    config_file_name = sys.argv[2]  # second argument after script name
+    all_cfgs = find_region_cfg(snapshots_root, config_file_name)
     for path in all_cfgs:
         print(path)
 
