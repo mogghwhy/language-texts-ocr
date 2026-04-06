ab2a6cf410146f49154e996314f23273e17d728f
mogghwhy <112501265+mogghwhy@users.noreply.github.com>
2026-04-06 15:11:02 +0300

    add get_configs_and_snapshots function to retrieve configurations and associated snapshot files

--- module/config_utils.py
+++ module/config_utils.py
@@ -24,3 +24,20 @@ def find_files(root_dir, file_name, search_recursive=True):
 def get_abs_parent_folder(abs_path):
     root = Path(abs_path)
     return root.parent.absolute()
+
+def get_configs_and_snapshots(root_dir, config_file_name, snapshot_file_extension=".jpg"):
+    configs = find_region_cfg(root_dir, config_file_name)
+    result = []
+    for config in configs:
+        config_root_dir = get_abs_parent_folder(config)
+        snapshots = find_snapshot_files(config_root_dir, snapshot_file_extension)
+        count = len(snapshots)
+        logger.info(f"Found {count} snapshot files in {config_root_dir} for config {config}")
+        if count:
+            dictionary = {
+                "config": config,
+                "snapshots": snapshots,
+                "snapshot_base_dir": config_root_dir
+            }
+            result.append(dictionary)
+    return result
