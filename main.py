77356f4ce099561e50776b4007158970933d7976
mogghwhy <112501265+mogghwhy@users.noreply.github.com>
2026-04-06 15:12:20 +0300

    refactor main function to use get_configs_and_snapshots for improved configuration and snapshot handling

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sys
-from module.config_utils import find_region_cfg, get_abs_parent_folder
+from module.config_utils import get_configs_and_snapshots
 
 def main():
 
@@ -9,10 +9,17 @@ def main():
 
     snapshots_root = sys.argv[1]    # first argument after script name
     config_file_name = sys.argv[2]  # second argument after script name
-    all_cfgs = find_region_cfg(snapshots_root, config_file_name)
-    for path in all_cfgs:
-        print(path)
-        print(get_abs_parent_folder(path))
+    configs_and_snapshots = get_configs_and_snapshots(snapshots_root, config_file_name)
+    for item in configs_and_snapshots:
+        print(f"Config: {item['config']}")
+        print(f"Snapshot base dir: {item['snapshot_base_dir']}")
+        print("Snapshots:")
+        for snapshot in item['snapshots']:
+            print(f"  - {snapshot}")
+    # all_cfgs = find_region_cfg(snapshots_root, config_file_name)
+    # for path in all_cfgs:
+    #     print(path)
+    #     print(get_abs_parent_folder(path))
 
 if __name__ == "__main__":
     main()
