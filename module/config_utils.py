ef3134587b4152d57d30b2936a596824348a3942
mogghwhy <112501265+mogghwhy@users.noreply.github.com>
2026-04-06 14:35:50 +0300

    extract logic from find_region_cfg into a separate reusable function

--- module/config_utils.py
+++ module/config_utils.py
@@ -1,5 +1,11 @@
 from pathlib import Path
 
 def find_region_cfg(root_dir, region_config_name):
+    return find_files(root_dir, region_config_name)
+
+def find_files(root_dir, file_name, search_recursive=True):
     root = Path(root_dir).resolve()
-    return [p.resolve() for p in root.rglob(region_config_name)]
+    if search_recursive:
+        return [p.resolve() for p in root.rglob(file_name)]
+    else:
+        return [p.resolve() for p in root.glob(file_name)]
